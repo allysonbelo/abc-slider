7afd898004a7059dee10d5b01f340744c3179fa6
allysonbelo <allysonbelo@gmail.com>
2023-01-17 19:35:05 -0300

    Add: Escapando informação

--- views/abc-slider_metabox.php
+++ views/abc-slider_metabox.php
@@ -1,10 +1,23 @@
+<?php 
+    $meta = get_post_meta( $post->ID );
+    $link_text = get_post_meta( $post->ID, 'abc_slider_link_text', true);
+    $link_url = get_post_meta( $post->ID, 'abc_slider_link_url', true);
+    var_dump($$link_text, $link_url);
+?>
 <table class="form-table abc-slider-metabox">
     <tr>
         <th>
             <label for="abc_slider_link_text">Link Text</label>
         </th>
         <td>
-            <input type="text" name="abc_slider_link_text" id="abc_slider_link_text" class="regular-text link-text" value="" required>
+            <input 
+                type="text" 
+                name="abc_slider_link_text" 
+                id="abc_slider_link_text" 
+                class="regular-text link-text" 
+                value="<?php echo (isset ($link_text) ) ? esc_html( $link_text ) : ''; ?>" 
+                required
+            >
         </td>
     </tr>
     <tr>
@@ -12,7 +25,14 @@
             <label for="abc_slider_link_url">Link Url</label>
         </th>
         <td>
-            <input type="url" name="abc_slider_link_url" id="abc_slider_link_url" class="regular-text link url" value="" required>
+            <input 
+                type="url" 
+                name="abc_slider_link_url" 
+                id="abc_slider_link_url" 
+                class="regular-text link url" 
+                value="<?php echo (isset($link_url)) ? esc_url( $link_url ) : ''; ?>" 
+                required
+            >
         </td>
     </tr>
 </table>
