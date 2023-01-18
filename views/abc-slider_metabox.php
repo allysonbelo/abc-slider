c860b1d42e21958dcb54e67bd0cb1e58d7b8f595
allysonbelo <allysonbelo@gmail.com>
2023-01-18 14:17:05 -0300

    Fix: Nonces e outras validações

--- views/abc-slider_metabox.php
+++ views/abc-slider_metabox.php
@@ -2,9 +2,10 @@
     $meta = get_post_meta( $post->ID );
     $link_text = get_post_meta( $post->ID, 'abc_slider_link_text', true);
     $link_url = get_post_meta( $post->ID, 'abc_slider_link_url', true);
-    var_dump($$link_text, $link_url);
+    // var_dump($$link_text, $link_url);
 ?>
 <table class="form-table abc-slider-metabox">
+    <input type="hidden" name="abc_slider_nonce" value="<?php echo wp_create_nonce('abc_slider_nonce') ?>">
     <tr>
         <th>
             <label for="abc_slider_link_text">Link Text</label>
