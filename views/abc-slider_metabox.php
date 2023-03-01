904dc6283a949424cc04dd379def2556bfb74561
allysonbelo <allysonbelo@gmail.com>
2023-03-01 15:51:32 -0300

    Add: Estilização de cores dos textos, link e botão do slide

--- views/abc-slider_metabox.php
+++ views/abc-slider_metabox.php
@@ -2,6 +2,10 @@
 $meta = get_post_meta($post->ID);
 $link_text = get_post_meta($post->ID, 'abc_slider_link_text', true);
 $link_url = get_post_meta($post->ID, 'abc_slider_link_url', true);
+$link_url_color = get_post_meta($post->ID, 'abc_slider_color_link', true);
+$link_text_color = get_post_meta($post->ID, 'abc_slider_color_text', true);
+$btn_color = get_post_meta($post->ID, 'abc_slider_color_btn', true);
+$description_color = get_post_meta($post->ID, 'abc_slider_color_description', true)
 //var_dump( $link_text, $link_url );
 ?>
 <table class="form-table abc-slider-metabox">
@@ -16,10 +20,42 @@ $link_url = get_post_meta($post->ID, 'abc_slider_link_url', true);
     </tr>
     <tr>
         <th>
+            <label for="abc_slider_color_text"><?php esc_html_e('Color text', 'abc-slider'); ?></label>
+        </th>
+        <td>
+            <input type="color" name="abc_slider_color_text" id="abc_slider_color_text" class="" value="<?php echo (isset($link_text_color)) ? esc_url($link_text_color) : ''; ?>">
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="abc_slider_color_description"><?php esc_html_e('Color description', 'abc-slider'); ?></label>
+        </th>
+        <td>
+            <input type="color" name="abc_slider_color_description" id="abc_slider_color_description" class="" value="<?php echo (isset($description_color)) ? esc_url($description_color) : ''; ?>">
+        </td>
+    </tr>
+    <tr>
+        <th>
             <label for="abc_slider_link_url"><?php esc_html_e('Link URL', 'abc-slider'); ?></label>
         </th>
         <td>
             <input type="url" name="abc_slider_link_url" id="abc_slider_link_url" class="regular-text link-url" value="<?php echo (isset($link_url)) ? esc_url($link_url) : ''; ?>" required>
+        </td>
+    </tr>    
+    <tr>
+        <th>
+            <label for="abc_slider_color_link"><?php esc_html_e('Color url', 'abc-slider'); ?></label>
+        </th>
+        <td>
+            <input type="color" name="abc_slider_color_link" id="abc_slider_color_link" class="" value="<?php echo (isset($link_url_color)) ? esc_url($link_url_color) : ''; ?>">
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="abc_slider_color_btn"><?php esc_html_e('Color btn', 'abc-slider'); ?></label>
+        </th>
+        <td>
+            <input type="color" name="abc_slider_color_btn" id="abc_slider_color_btn" class="" value="<?php echo (isset($btn_color)) ? esc_url($btn_color) : ''; ?>">
         </td>
     </tr>
     <tr>
@@ -27,15 +63,7 @@ $link_url = get_post_meta($post->ID, 'abc_slider_link_url', true);
             <label for="abc_slider_link_image"><?php esc_html_e('Slide ID', 'abc-slider'); ?></label>
         </th>
         <td>
-            <input 
-                type="url" 
-                disabled
-                name="abc_slider_link_image" 
-                id="abc_slider_link_image" 
-                class="" 
-                required
-                value=" <?php the_ID(); ?> " 
-            >
+            <input type="url" disabled name="abc_slider_link_image" id="abc_slider_link_image" class="" required value=" <?php the_ID(); ?> ">
         </td>
     </tr>
 </table>
